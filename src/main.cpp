6e5a93c3d888ebb6bfa733e69478fd6749bb8b0d
PEDRO LEONEL DE LORENA <53955089886@info.local>
2026-05-27 15:38:42 -0300

    Versão 0.0.4: Adicionado o esboço das funções de todos os Botões, alem de ter começado a logica do tempo

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
  *  Descrição: Neste modulo de publish nos coletamos os hex do controle remoto e fizemos com que toda vez que apertasse o botao ele emitsse para a tela oq o hex faz.
  *  Projeto: Modulo de publicar os comandos feito na tela retratil
  *  Data: 21/05
- *  Versão: 0.0.3
+ *  Versão: 0.0.4
  */
 
 #include <Arduino.h>
@@ -23,17 +23,15 @@
 //=========================
 //* Variaveis globais
 
-String hexUP = "codigo do hexUP";
-String hexDOWN = "codigo do hexDOWN";
-String hexSELECT = "codigo do hexSELECT";
+String frequenciaUP = "frequenciaUP";
+String frequenciaDOWN = "frequenciaDOWN";
+String frequenciaPAUSE = "frequenciaPAUSE";
 
 const unsigned long tempoInicio = 0;
-
-unsigned long tempo = millis();
+unsigned long tempo = 0;
+unsigned long tempoDecorrido = 0;
 unsigned long tempoPause;
-
 unsigned long tempoMaximo = 20000;
-
 bool tempoParado = false;
 //=========================
 
@@ -81,27 +79,77 @@ void loop()
   }
 }
 
+
+/*
+  A ideia é que o botaoUP e down tenha a mesma logica pois ambos tem a mesma função, só que um sobe e o outro desce, e o botaoPAUSE tem a função de pausar o tempo, ou seja, quando ele for apertado ele salva o tempo decorrido e para de contar, e quando ele for apertado novamente ele continua contando de onde parou.
+*/
 void botaoUP()
 {
-  if (hexUP == "")
-    debugErro("O código do hex está errado, favor checar se esta correto");
+  if (frequenciaUP == "")
+    debugErro("A frequência do botão UP está errada, favor checar se está correta");
 
-  if (hexUP == "codigo do hexUP")
+  if (frequenciaUP == "frequencia do botão UP")
   {
     // TODO VER QUANTO TEMPO ELA DEMORA PRA SUBIR POR COMPLETO E FAZER UM CRONOMETTRO Q QUANDO ELA SUBIR TUDO VAI EXIVIR
     // TODO FAZER UMA VARIAVEL DE PAUSE Q SALVA AONDE O MILLIS PAROU POIS QND ELE APERTAR O PAUSE O MILLIS NAO PODE CONTINUAR ANDANDO
     if (tempoParado)
     {
       debugInfo("Tela retrátil subindo");
+      tempo = millis() - tempoPause;
+      tempoParado = false;
 
-      if (tempo - tempoInicio >= tempoMaximo)
-      {
-        debugInfo("Tela retrátil subiu por completo.");
-      }
+      // Serial para debug
+    debugInfo("Tempo decorrido: " + String(tempoDecorrido));
+    debugInfo(" ms");
     }
+    tempoDecorrido = millis() - tempo;
 
-    String texto;
-    serializeJson(doc, texto);
-    publicarMensagem(TOPICO_COMANDO, texto.c_str()); // ver como faz pra trocar o topico_comando pelo topico_publicar
+    if (tempoDecorrido>= tempoMaximo)
+    {
+      debugInfo("Tela retrátil subiu por completo.");
+      tempoDecorrido = 0;
+      tempoParado = false;
+    }
+  }
+}
+
+void botaoPause()
+{
+  if (frequenciaPAUSE == "")
+    debugErro("A frequência do botão Pause está errada, favor checar se está correta");
+
+  if (frequenciaPAUSE == "frequencia do botão PAUSE")
+  {
+    debugInfo("Tela retrátil pausada");
+    tempoParado = true;
+    tempoPause = tempoDecorrido;
+
+  }
+}
+
+void botaoDOWN()
+{
+  if (frequenciaDOWN == "")
+    debugErro("A frequência do botão DOWN está errada, favor checar se está correta");
+
+  if (frequenciaDOWN == "frequencia do botão DOWN")
+  {
+    if (tempoParado)
+    {
+      debugInfo("Tela retrátil descendo");
+      tempo = millis() - tempoPause;
+      tempoParado = false;
+
+       // Serial para debug
+    debugInfo("PAUSADO em: " + String(tempoPause) + " ms");
+    }
+    tempoDecorrido = millis() - tempo;
+
+    if (tempoDecorrido >= tempoMaximo)
+    {
+      debugInfo("Tela retrátil desceu por completo.");
+      tempoDecorrido = 0;
+      tempoParado = false;
+    }
   }
 }
