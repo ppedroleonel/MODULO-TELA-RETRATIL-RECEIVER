c009d7ab78c959dda3fb1218ba805d2766499bb6
pedro <pedro.leonel876@gmail.com>
2026-06-04 17:37:17 -0300

    V 1.1.0: Arrumado o cabeçalho pois estava desatualizado

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,23 @@
 /**
  *   Nome: Pedro Leonel de Lorena, Leonardo Ferrarese Correa, Lais Rodrigues Sevilhano & Luigi Arnosti Reginato
- *  Descrição: Neste modulo de publish nos coletamos os hex do controle remoto e fizemos com que toda vez que apertasse o botao ele emitsse para a tela oq o hex faz.
- *  Projeto: Modulo de publicar os comandos feito na tela retratil
- *  Data: 01/06/2026
- *  Versão: 1.0.2
+ *  Descrição: Neste módulo de receiver, o ESP32 recebe comandos JSON via MQTT e converte esses comandos em ações RF
+ *             para controlar as telas retráteis, encaminhando UP, DOWN e PAUSE conforme o comando recebido.
+ *  Projeto: Módulo de controle de tela retrátil via AWS IoT e RF
+ *  Data: 04/06/2026
+ *  Versão: 1.1.0
  *  */
 
 #include <Arduino.h>
 #include <ESP32Connectivity.h>
 #include <Conectividade.h>
 #include <DebugManager.h>
-#include <ArduinoJson.h>
-#include <secrets.h>
-#include <Bounce2.h>
-#include <TelaProjecaoRF.h>
+#include "secrets.h"
 #include "tratarJsonComando.h"
 #include "callbackManager.h"
 
+// Inicialização do objeto de conectividade MQTT
 ESP32Connectivity conexao;
 
-// Inicializa a conexão Wi-Fi e MQTT, registra callbacks para eventos de conectividade,
-// e inicia o módulo RF para captura de comandos do controle remoto
 void setup()
 {
   Serial.begin(115200);
