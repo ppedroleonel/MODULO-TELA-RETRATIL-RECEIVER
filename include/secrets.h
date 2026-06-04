0710357dce2a9e6ada23187f94bd73e292533696
pedro <pedro.leonel876@gmail.com>
2026-06-04 17:47:15 -0300

    V 1.1.0: Criado as variaveis para armazenar as credenciais sensiveis como SSID e certificados da AWS

--- include/secrets.h
+++ include/secrets.h
@@ -1,65 +1,37 @@
-//!secrets.
+//! secrets.h
+// Este arquivo armazena as credenciais e configurações sensíveis, como SSID, senha Wi-Fi, certificados AWS IoT e tópicos MQTT. Ele é incluído em outros arquivos para acessar essas informações de forma centralizada.
 
 #ifndef SECRETS_H
 #define SECRETS_H
 
-// * CONFIGURAÇÕES DO WIFI
-
 extern const char *WIFI_SSID;
 extern const char *WIFI_SENHA;
 
-//======================================
-// *MQTT
-//======================================
+extern const char *awsCertCA;
 
-extern const bool USAR_AWS_IOT;
+extern const char *awsCertCRT;
 
-extern const char* MQTT_BROKER;
-extern const int MQTT_PORTA;
+extern const char *awsCertPrivate;
 
-// o ID do mqtt sempre vai ser diferente
-// nunca criar um id igual de alguem pois ele nao permite e fica desconectando um
-extern const char* MQTT_CLIENT_ID;
+extern const char *pub[];
 
-extern const char* MQTT_USUARIO;
-extern const char* MQTT_SENHA;
-
-// essa extern const fala q nao estamos usando certificado
-// modo de conexao sem segurança
-extern const bool MQTT_USAR_TLS;
-
-extern const  char MQTT_CERTIFICADO_CA[];
-extern const char* TOPICOS_PUBLICAR[];
-
-// * AWS
-
-extern const char AWS_CERT_CA[];
-extern const char AWS_CERT_CRT[];
-extern const char AWS_CERT_PRIVATE[];
-extern const char* AWS_IOT_ENDPOINT;
-extern const int AWS_IOT_PORT;
-extern const char* AWS_IOT_CLIENT_ID;
-
-
-
-extern const int TOTAL_TOPICOS_PUBLICAR;
-
-// 
-extern const char* TOPICOS_RECEBER[];
-
-extern const int TOTAL_TOPICOS_RECEBER;
+extern const char *rec[];
 
 //======================================
 // *DEBUG
 //======================================
 
-// 0 = sem mensagens
-// 1 = apenas erros
-// 2 = todas mensagens
+/*
+0	DEBUG_NENHUM | Nada
+1	DEBUG_ERRO	| Falhas críticas
+2	DEBUG_AVISO	| Reconexões e timeouts
+3	DEBUG_INFO	| Fluxo normal
+4	DEBUG_VERBOSE	| Tentativas e estados internos
+5	DEBUG_TUDO | Payloads e códigos de erro brutos
+*/
 extern const int DEBUG_NIVEL_INICIAL;
 
 // Pino usado para forçar todas as mensagens na inicialização
 extern const int PINO_HABILITAR_DEBUG_COMPLETO;
-
 
 #endif
