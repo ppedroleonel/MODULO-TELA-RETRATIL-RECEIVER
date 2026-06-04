ebfe87bc4bbef1827ccec433247647524070faf7
pedro <pedro.leonel876@gmail.com>
2026-06-04 17:44:23 -0300

    V 1.1.0: Adicionado as credenciais e declarado as estrutras de função

---
+++ include/conectividade.h
@@ -0,0 +1,14 @@
+//! conectividade.h
+// Este arquivo declara as estruturas de configuração para Wi-Fi, AWS IoT e tópicos MQTT, além de externar as variáveis de configuração definidas em Conectividade.cpp
+
+#ifndef conectividade_h
+#define conectividade_h
+
+#include <Arduino.h>
+#include <ESP32Connectivity.h>
+
+extern ConfigWiFi wifiConfig;
+extern ConfigAWS awsConfig;
+extern ConfigTopicos topicosConfig;
+
+#endif
--- src/conectividade.cpp
+++ src/conectividade.cpp
@@ -1,5 +1,5 @@
 //! Conectividade.cpp
-// Este arquivo configura as credenciais e parâmetros necessários para conectar ao AWS IoT
+// Este arquivo configura as credenciais e parâmetros necessários para conectar ao AWS IoT, incluindo Wi-Fi, certificados e tópicos MQTT.
 
 #include <Arduino.h>
 #include <ESP32Connectivity.h>
@@ -12,11 +12,12 @@ ConfigWiFi wifiConfig = {
     WIFI_SENHA
 };
 
-// Configuração AWS IoT:
-// - Endpoint do broker MQTT AWS
-// - Porta 8883 (MQTT com TLS)
-// - ID do cliente
-// - Certificados CA, cliente e chave privada (carregados do arquivo secrets.h)
+/* Configuração AWS IoT:
+ - Endpoint do broker MQTT AWS
+ - Porta 8883 para conexões seguras TLS
+ - ID do cliente
+ - Certificados CA, cliente e chave privada (inseridos do arquivo secrets.h)
+ */
 ConfigAWS awsConfig = {
     "a6t7fzch59vsw-ats.iot.us-east-1.amazonaws.com",
     8883,
@@ -26,9 +27,10 @@ ConfigAWS awsConfig = {
     awsCertPrivate
 };
 
-// Configuração dos tópicos MQTT:
-// - pub: tópicos para publicar (nivel de QoS = 1)
-// - rec: tópicos para receber (nivel de QoS = 1)
+/* Configuração dos tópicos MQTT:
+  - pub: tópicos para publicar total de topicos 1
+  - rec: tópicos para receber, total de topicos 1
+  */
 ConfigTopicos topicosConfig = {
     pub, 1,
     rec, 1
